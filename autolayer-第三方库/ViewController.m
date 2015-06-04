021793bb3f9da2a6048267947d09622d050bfe06
潘阳君 <panyangjun1@panyangjun.local>
2015-06-05 00:53:18 +0800

    使用Masonry框架来进行autolayer布局。只添加了两个简单的操作。

--- autolayer-第三方库/ViewController.m
+++ autolayer-第三方库/ViewController.m
@@ -5,8 +5,9 @@
 //  Created by 潘阳君 on 15/6/4.
 //  Copyright (c) 2015年 潘阳军. All rights reserved.
 //
-
 #import "ViewController.h"
+#import <Masonry.h>
+
 
 @interface ViewController ()
 
@@ -14,14 +15,33 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    UIView *view1 = [[UIView alloc] init];
+    view1.backgroundColor = [UIColor lightGrayColor];
+    [self.view addSubview:view1];
+    UIView *view2 = [[UIView alloc] init];
+    view2.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:view2];
+    //添加约束
+    [view1 mas_makeConstraints:^(MASConstraintMaker *make)
+    {
+        make.right.equalTo(view2.mas_left).with.offset(-20);
+        make.top.equalTo(self.view.mas_top).with.offset(50);
+        make.height.equalTo(view2);
+        make.left.equalTo(self.view.mas_left).with.offset(10);
+        
+
+    }];
+    [view2 mas_makeConstraints:^(MASConstraintMaker *make)
+    {
+        make.right.equalTo(self.view.mas_right).with.offset(-10);
+        make.top.equalTo(view1);
+        
+    }];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
